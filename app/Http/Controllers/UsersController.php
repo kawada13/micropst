aad1bcb5a8f1ceca2df9e34ab80348caf962e140
kawada <vgnsl94@gmail.com>
2021-08-05 15:40:54 +0900

    s

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -21,7 +21,9 @@ class UsersController extends Controller
     public function show($id)
     {
         $user = User::find($id);
+
         $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(5);
+
 
         $data = [
             'user' => $user,
